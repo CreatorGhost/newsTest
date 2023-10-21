6088aa9bdfa3f7cdfceb8270e53f170adfbc8c1c
Aditya Pratap Singh <cybercreed010@gmail.com>
2023-10-21 14:01:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,25 +10,27 @@ import openai
 import redis
 from scrap import main
 
+
 def configure_app():
     load_dotenv()
-    openai_api_key = os.getenv('OPENAI_API_KEY')
+    openai_api_key = os.getenv("OPENAI_API_KEY")
     if not openai_api_key:
         logging.error("OPENAI_API_KEY not found in environment variables.")
         raise ValueError("OPENAI_API_KEY not found in environment variables.")
     openai.api_key = openai_api_key
     embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
-    redis_instance = redis.Redis(host="127.0.0.1", port="6379")
-    return embeddings, redis_instance
+    return embeddings
+
 
 path = "./clean.csv"
+
 
 def check_file(path):
     if not os.path.isfile(path):
         main(path)
 
 
-def vectorize(index,embeddings):
+def vectorize(index, embeddings):
     loader = CSVLoader(path)
     data = loader.load()
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=50)
@@ -36,50 +38,54 @@ def vectorize(index,embeddings):
     Redis.from_documents(
         texts,
         embeddings,
-        redis_url = "redis://127.0.0.1:6379",
-        index_name = index
+        redis_url=os.getenv("REDIS_URL"),
+        password=os.getenv("REDIS_PASSWORD"),
+        index_name=index,
     )
     document_store = Redis.from_existing_index(
-            index_name = index,
-            redis_url = "redis://127.0.0.1:6379",
-            embedding = embeddings
-        )
+        index_name=index,
+        redis_url=os.getenv("REDIS_URL"),
+        password=os.getenv("REDIS_PASSWORD"),
+        embedding=embeddings,
+    )
     return document_store
 
-def load_data(index,embeddings):
+
+def load_data(index, embeddings):
     print("Loading Index")
     try:
         document_store = Redis.from_existing_index(
-            index_name = index,
-            redis_url = "redis://127.0.0.1:6379",
-            embedding = embeddings
+            index_name=index,
+            redis_url=os.getenv("REDIS_URL"),
+            password=os.getenv("REDIS_PASSWORD"),
+            embedding=embeddings,
         )
         print(f"Data loaded from existing index {index}")
     except:
-        document_store = vectorize(index,embeddings)
+        document_store = vectorize(index, embeddings)
         print(f"Data inserted for index {index}")
     return document_store
 
-def perform_search(ques,data_store):
+
+def perform_search(ques, data_store):
     data_store = data_store
-    related_doc = data_store.similarity_search(ques,k=3)
-    content = [ doc.page_content for doc in related_doc]
+    related_doc = data_store.similarity_search(ques, k=3)
+    content = [doc.page_content for doc in related_doc]
     knowledge = "\n".join(content)
-    query =  "Based on the below knowledge answer this question." + ques + "\n" + knowledge
+    query = (
+        "Based on the below knowledge answer this question." + ques + "\n" + knowledge
+    )
 
     chat = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages = [
-                {
+        model="gpt-3.5-turbo",
+        messages=[
+            {
                 "role": "system",
-                "content": "You are an incredible AI who can answer any question accurately, in an unique way."
-                },
-                {
-                    "role": "user",
-                    "content" : query
-                }
-            ]
-        )
+                "content": "You are an incredible AI who can answer any question accurately, in an unique way.",
+            },
+            {"role": "user", "content": query},
+        ],
+    )
 
     ans = chat["choices"][0]["message"]["content"]
     return ans
@@ -88,25 +94,27 @@ def perform_search(ques,data_store):
 def create_app():
     app = Flask(__name__)
     check_file(path)
-    embeddings, redis_instance = configure_app()
-    data_store = load_data("cryptonews",embeddings)
-    return app, path, embeddings, redis_instance ,data_store
+    embeddings = configure_app()
+    data_store = load_data("cryptonews", embeddings)
+    return app, path, embeddings, data_store
 
-app, path, embeddings, redis_instance ,data_store = create_app()
 
-@app.route('/search-news', methods=['GET','POST'])
+app, path, embeddings,  data_store = create_app()
+
+
+@app.route("/search-news", methods=["GET", "POST"])
 def search_pdf():
-    if request.method == 'POST':
-        search_query = request.form['query']
-        results = perform_search(search_query,data_store)
-        return render_template('index.html',result=results)
+    if request.method == "POST":
+        search_query = request.form["query"]
+        results = perform_search(search_query, data_store)
+        return render_template("index.html", result=results)
     else:
-        return render_template('index.html')
+        return render_template("index.html")
 
 
-
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     app.run(debug=True)
+
+
+
